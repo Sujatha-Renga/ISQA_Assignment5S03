58194f018b3afc3ee20cd34c9f061923c0a690c7
Sujatha-Renga <89357383+Sujatha-Renga@users.noreply.github.com>
2022-05-03 23:29:50 -0500

    ff
    ff

--- MAH_V5/settings.py
+++ MAH_V5/settings.py
@@ -124,21 +124,18 @@ AUTH_USER_MODEL = 'user.ExtUser'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
-# LOGIN_URL = '/user/login/'
 
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+LOGIN_REDIRECT_URL = '/'
+
 STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'studentscafe.omaha@gmail.com'
